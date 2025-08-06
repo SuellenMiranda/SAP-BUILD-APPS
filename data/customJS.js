a4307f16c6d83ce7158c18551993e790e3ffe30b
Suh <suellen.org@gmail.com>
2025-08-06 17:04:26 -0300

    Update customJS.js

--- data/customJS.js
+++ data/customJS.js
@@ -1,1 +1,15 @@
-
+//return { result: inputs.input1.toUpperCase() };
+// Code working
+return {
+  result: (function(msString) {
+    //function
+    const ms = parseInt(msString.match(/\d+/)[0], 10);
+    const date = new Date(ms);
+    //day,month,year
+    const day = String(date .getUTCDate()).padStart(2, '0') ;
+    const month = String(date.getUTCMonth() + 1).padStart(2, '0'); // JANUARY IS 0
+    const year = date.getUTCFullYear();
+    //result
+    return `${day}/${month}/${year}`;
+  })(inputs.input1)
+};
